1bc336df59cd57a34f091a59b5ef4064a1fdb496
alfredohuerta <eagarciahuerta@hotmail.com>
2024-02-15 18:20:49 -0600

    Added new project/homework and configurated cmake project

---
+++ a01708119_Act1-1/main.cpp
@@ -0,0 +1,22 @@
+/**
+* @author: Alfredo García Huerta - A01708119 - @alfredohuerta
+ *
+ * @name:Actividad 1.1 Implementación de la técnica de programación "divide y vencerás"
+ *
+ * @details: Utilizando la técnica de programación de "divide y vencerás", escribe en equipos de máximo dos personas, en
+ * C++ un programa que implemente el algoritmo de MergeSort El programa recibe un numero entero N, seguido de N valores
+ * reales (double, uno en cada línea) por la entrada estándar. La salida del programa es una lista de los N valores
+ * ordenados de Mayor a menor
+ *
+ * @date: 15 - febrero - 2024
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello world";
+
+    return 0;
+}
